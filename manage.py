dafe4bf4903c65afddf2d20f43d615ab4bae1518
Jakob <jakobpuri02@gmail.com>
2025-08-22 23:14:03 +0800

    Correct project structure for settings.py

--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoProject.settings')
+    # In manage.py
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hauspital.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
