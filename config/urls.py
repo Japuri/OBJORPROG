3c7823bc1a95e975460d4c9c92558b4ef4d0c643
Jakob <jakobpuri02@gmail.com>
2025-08-23 18:03:13 +0800

    Fix app name in main urls.py

--- config/urls.py
+++ config/urls.py
@@ -5,8 +5,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
-    path('accounts/', include('users.urls')), # <--- Include your users app URLs here
+    path('', include('myapp.urls')),  # <--- Include your users app URLs here
     # You can also include Django's default auth URLs if you prefer, but we're customizing
     # path('accounts/', include('django.contrib.auth.urls')),
 ]
