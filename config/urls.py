964a2a3bcaae494261bee48da37e9bd921f4ab22
Jakob <jakobpuri02@gmail.com>
2025-08-23 18:14:49 +0800

    Route root URL to myapp

--- config/urls.py
+++ config/urls.py
@@ -8,5 +8,5 @@ from django.urls import path, include # 1. Make sure 'include' is imported
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls')), # 2. Add this line to include your app's URLs
+    path('', include('myapp.myapp.urls')), # 2. Add this line to include your app's URLs
 ]
