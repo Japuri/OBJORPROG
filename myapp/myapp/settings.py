2c1550d2f354ad94eb44745b6584e2489338e068
Jakob Puri <jakobpuri02@gmail.com>
2025-08-30 11:22:14 +0800

    edited procfile

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -30,7 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp'
+    'myapp',
     'main',
     'users',
     'crispy_forms',
